367591c1e2f072e95f8c82281843fa567226d8ce
LZP <worklzp@163.com>
2018-02-22 17:27:20 +0800

    样式修改

--- application/config.php
+++ application/config.php
@@ -141,6 +141,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         '__STATIC__'=>str_replace('/index.php','',\think\Request::instance()->root()).'/public/home',
+        '__ADMIN__'=>str_replace('/index.php','',\think\Request::instance()->root()).'/public/admin',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
